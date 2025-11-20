5901828e5aebd0772b5ded4ecc9886ef25da6d02
akshaykumarpatil33 <akshaykumarpatil33@gmail.com>
2025-11-20 14:29:59 +0530

    made all respective changes for communication among services

--- frontend/verification-app/vite.config.ts
+++ frontend/verification-app/vite.config.ts
@@ -5,12 +5,12 @@ export default defineConfig({
   plugins: [react()],
   server: {
     port: 3003,
-    proxy: {
-      '/api': {
-        target: 'http://localhost:3001',
-        changeOrigin: true,
-      },
-    },
+    // proxy: {
+    //   '/api': {
+    //     target: 'http://localhost:3001',
+    //     changeOrigin: true,
+    //   },
+    // },
   },
   build: {
     outDir: 'dist',
