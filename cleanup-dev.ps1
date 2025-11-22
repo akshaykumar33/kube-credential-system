92bfdce486c8ad119340685e52239e8053ae9321
akshaykumarpatil33 <akshaykumarpatil33@gmail.com>
2025-11-22 14:16:38 +0530

    cleanup added for killing ports in local windows m/c

--- cleanup-dev.ps1
+++ cleanup-dev.ps1
@@ -1,5 +1,5 @@
 # cleanup-dev.ps1
 Write-Host "Stopping all Node processes and freeing dev ports..."
 taskkill /F /IM node.exe
-npx kill-port 7000 7001 7002 7003
-Write-Host "Done! Ports 7000-7003 are free."
+npx kill-port 3000 3001 3002 3003
+Write-Host "Done! Ports 3000-3003 are free."
